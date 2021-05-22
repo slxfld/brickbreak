b866c34550a68d9c5a0c31f7a34e1b1113359355
slxfld <31144712+slxfld@users.noreply.github.com>
2021-05-22 19:17:04 +0200

    Add Tilemap Files

---
+++ C++/BrickBreak_VSCode/data/Bricks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Bricks" tilewidth="16" tileheight="16" tilecount="7" columns="7">
+ <image source="colors.png" width="112" height="16"/>
+</tileset>
